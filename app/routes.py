cabc46df646d5ceb26193ff2a27e64a865cc9fdf
Tommy Herbert <tommyherbert@gmail.com>
2018-05-31 15:13:21 +0100

    Conversation object; HTML markup.

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,16 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'My name is Ox.'
+    first_utterance = {'text': 'Hello, my name is Ox.'}
+    conversation = {'first_utterance': first_utterance}
+    return '''
+<html>
+    <head>
+        <title>Ox</title>
+    </head>
+    <body>
+        <h1>''' + conversation['first_utterance']['text'] + '''</h1>
+    </body>
+</html>
+'''
 
