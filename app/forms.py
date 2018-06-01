e928f217bcaab2bc9b126889bae461f20ba48014
Tommy Herbert <tommyherbert@gmail.com>
2018-06-01 15:57:27 +0100

    Sign-in form.

---
+++ app/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class SignInForm(FlaskForm):
+    email = StringField('email address', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    sign_up = SubmitField('sign up')
+    sign_in = SubmitField('sign in')
+
