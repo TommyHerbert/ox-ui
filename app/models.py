474790d36a3b7798bc209aa4bebf46aca1309649
Tommy Herbert <tommyherbert@gmail.com>
2018-06-05 14:00:34 +0100

    Utterance and conversation tables

--- app/models.py
+++ app/models.py
@@ -9,3 +9,15 @@ class Speaker(db.Model):
     def __repr__(self):
         return '<Speaker {}>'.format(self.email)
 
+
+class Utterance(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(128))
+    speaker_id = db.Column(db.Integer, db.ForeignKey('speaker.id'))
+    next_id = db.Column(db.Integer, db.ForeignKey('utterance.id'))
+
+
+class Conversation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_utterance_id = db.Column(db.Integer, db.ForeignKey('utterance.id'))
+
