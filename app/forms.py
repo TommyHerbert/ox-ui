3447c2712e8f9904d85338b27932c1f4195426b3
Tommy Herbert <tommyherbert@gmail.com>
2018-06-06 16:51:37 +0100

    Use TextAreaField for talking to Ox.

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms.validators import \
+    DataRequired, ValidationError, Email, EqualTo, Length
 from app.models import Speaker
 
 
@@ -25,6 +26,7 @@ class SignUpForm(FlaskForm):
 
 
 class SaySomethingForm(FlaskForm):
-    text = StringField('text', validators=[DataRequired()])
+    text_validators = [DataRequired(), Length(min=1, max=128)]
+    text = TextAreaField('text', validators=text_validators)
     send = SubmitField('send')
 
