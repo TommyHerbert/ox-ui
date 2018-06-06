49f2bb328d7c28333fcfb47d6fa46139919c2aec
Tommy Herbert <tommyherbert@gmail.com>
2018-06-06 13:39:24 +0100

    Password logic.

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class Speaker(db.Model):
@@ -8,6 +9,12 @@ class Speaker(db.Model):
 
     def __repr__(self):
         return '<Speaker {}>'.format(self.email)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Utterance(db.Model):
