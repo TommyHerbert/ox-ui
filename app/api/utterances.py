1c1d6937b2b14a34ed428fc93333bc4682ecdcac
Tommy Herbert <tommyherbert@gmail.com>
2018-07-04 10:55:38 +0100

    WIP: API endpoint for creating a new utterance.

--- app/api/utterances.py
+++ app/api/utterances.py
@@ -1,8 +1,14 @@
 from app.api import bp
 from app.api.auth import token_auth
+from flask import request, jsonify, g
+
 
 @bp.route('/utterances', methods=['POST'])
 @token_auth.login_required
-def post_utterance(data):
-    pass # TODO
+def post_utterance():
+    data = request.get_json() or {}
+    for field in ['conversation_id', 'speaker_id', 'text']:
+        if field not in data:
+            return bad_request('missing one or more fields')
+    # TODO
 
