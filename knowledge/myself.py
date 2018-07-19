bc906019bd222dfe100ee2ea6aeecbd5ae3f7b5a
Tommy Herbert <tommyherbert@gmail.com>
2018-07-19 10:48:42 +0100

    Initialise the Myself concept as a Thing and give it a lexical form.

--- knowledge/myself.py
+++ knowledge/myself.py
@@ -3,5 +3,10 @@ from knowledge.concept import Thing
 
 class Myself(Thing):
     def __init__(self):
+        Thing.__init__(self)
+        self.lexical_form = 'Ox'
+
+        # Nothing is using this yet, and anyway Ox can't yet understand
+        # it. But it seems like a good way to set out our stall.
         self.goal = 'do good by teaching'
 
