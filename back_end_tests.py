0ca4bd90a753e064e8ba6c65e30d9472aee56a80
Tommy Herbert <tommyherbert@gmail.com>
2018-07-24 10:18:15 +0100

    Test for Ox's goal.

--- back_end_tests.py
+++ back_end_tests.py
@@ -4,6 +4,7 @@ from app import create_app, db
 from app.models import Speaker, Conversation, Utterance
 from config import Config
 from conversation.mind import Mind
+from knowledge.myself import Myself
 
 
 class TestConfig(Config):
@@ -22,17 +23,20 @@ class BackEndTests(unittest.TestCase):
         for s in [self.ox, self.test_user]:
             db.session.add(s)
         db.session.commit()
-        
+
         self.conversation = Conversation.create()
         for s in [self.ox, self.test_user]:
             self.conversation.speakers.append(s)
         db.session.add(self.conversation)
         db.session.commit()
-    
+
     def tearDown(self):
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
+
+    def test_goal(self):
+        self.assertEqual('do good by teaching', Myself().goal)
 
     def test_song(self):
         self._assert_response("What's your favourite song?", 'Hello')
@@ -69,7 +73,7 @@ class BackEndTests(unittest.TestCase):
                               conversation=self.conversation)
         db.session.add(utterance)
         db.session.commit()
-        
+
         Mind().continue_conversation(self.conversation)
         self.assertEqual(response, self.conversation.utterances[-1].text)
 
