97db84d682a9d968d238563b2c056fdf15786b0f
Tommy Herbert <tommyherbert@gmail.com>
2018-07-24 16:21:27 +0100

    Fix bugs in utterances API.

--- app/api/utterances.py
+++ app/api/utterances.py
@@ -1,7 +1,9 @@
+from app import db
 from app.api import bp
 from app.api.auth import token_auth
 from flask import request, jsonify, g
 from conversation.mind import Mind
+from app.models import Conversation, Utterance
 
 
 @bp.route('/utterances', methods=['POST'])
@@ -11,11 +13,12 @@ def post_utterance():
     for field in ['conversation_id', 'speaker_id', 'text']:
         if field not in data:
             return bad_request('missing one or more fields')
-    conversation = Conversation.query.get(id)
+    conversation = Conversation.query.get(data['conversation_id'])
     if not conversation:
         return error_response(404, 'no such conversation')
     utterance = Utterance(speaker_id=data['speaker_id'], text=data['text'])
     conversation.add_utterance(utterance)
     Mind().continue_conversation(conversation)
     db.session.commit()
+    return jsonify(conversation.to_dict())
 
